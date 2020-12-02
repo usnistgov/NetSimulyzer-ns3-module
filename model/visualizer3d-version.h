2e71d95cc178a65721b9e28579988707c3a060ad
Evan Black <evan.black@nist.gov>
2020-12-02 17:01:22 -0500

    Version 0.3.0

--- model/visualizer3d-version.h
+++ model/visualizer3d-version.h
@@ -48,7 +48,7 @@ const long VERSION_MAJOR = 0L;
 /**
  * The Minor version number for the module
  */
-const long VERSION_MINOR = 2L;
+const long VERSION_MINOR = 3L;
 
 /**
  * The Patch version number for the module
