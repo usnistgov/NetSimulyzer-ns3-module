1720aacc332549078c18f25d4905fbf6bfaf399f
Evan Black <evan.black@nist.gov>
2020-12-02 22:47:49 -0500

    Update documentation to 0.3.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ project = 'visualizer3d'
 author = 'Evan Black'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
