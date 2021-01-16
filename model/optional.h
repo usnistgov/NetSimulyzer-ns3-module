90c970049e453cabe7172f45627c3d27746ff333
Evan Black <evan.black@nist.gov>
2021-01-16 14:56:56 -0500

    Use std::optional for OptionalValue & docs

--- model/optional.h
+++ model/optional.h
@@ -42,197 +42,218 @@
 #include <memory>
 #include <typeinfo>
 #include <utility>
+#include <optional>
 
-namespace ns3 {
-namespace visualizer3d {
+namespace ns3::visualizer3d {
 
+/**
+ * Attribute wrapper for std::optional,
+ * a type to represent a value which may or may not be present.
+ *
+ * \tparam T
+ * The type of value to store within the attribute
+ *
+ * \see https://en.cppreference.com/w/cpp/utility/optional
+ */
 template <typename T>
-class Optional
+class OptionalValue : public AttributeValue
 {
 public:
-  Optional () = default;
-  Optional (const Optional<T> &other) = default;
+  /**
+   * Initialize the attribute with no stored value
+   */
+  OptionalValue (void) = default;
 
-  Optional<T> &operator= (const Optional<T> &other);
-  Optional<T> &operator= (T &&other);
+  /**
+   * Make a copy of `other`
+   *
+   * \param other
+   * The attribute to copy, will also copy the stored value
+   */
+  OptionalValue (const OptionalValue<T> &other) = default;
 
-  template <class... Args>
-  explicit Optional (Args &&...args);
+  /**
+   * Initialize the attribute directly from a std::optional.
+   * Allows for implicit conversion from a std::optional of
+   * the same type.
+   *
+   * \param value
+   * The value to copy into the new attribute.
+   */
+  OptionalValue (const std::optional<T> &value);
 
-  ~Optional ();
+  /**
+   * Constructs the contained type in-place, avoiding copies.
+   *
+   * An example using the `Color3` class follows:
+   * \code{.cc}
+   * // Calls Color3::Color3(uint8_t, uint8_t, uint8_t)
+   * // Never copies the color
+   * OptionalValue<Color3> optional{255u, 0u, 0u};
+   * \endcode
+   *
+   * \tparam Args (Deduced)
+   * The type of each argument passed
+   *
+   * \param args
+   * The arguments normally passed to the enclosing type's constructor.
+   */
+  template <typename... Args>
+  explicit OptionalValue (Args &&...args);
 
-  template <class... Args>
+  /**
+   * Sets the contained std::optional
+   * by copying `value`. Will overwrite
+   * the previously contained value.
+   *
+   * \param value
+   * The value to copy into this attribute.
+   */
+  void Set (const std::optional<T> &value);
+
+  /**
+   * Constructs the value of the std::optional in-place, avoiding copies.
+   * See the emplace constructor for more info
+   *
+   * \tparam Args (Deduced)
+   * The type of each argument passed
+   *
+   * \param args
+   * The arguments normally passed to the enclosing type's constructor.
+   */
+  template <typename... Args>
   void Emplace (Args &&...args);
 
-  const T &GetValue (void) const;
-  const T &operator* (void) const;
+  /**
+   * Retrieves a reference to the contained std::optional,
+   * not the actual contained value.
+   *
+   * \return
+   * A reference to the contained `std::optional`
+   *
+   * \see OptionalValue::GetValue
+   */
+  const std::optional<T> &Get (void) const;
 
+  /**
+   * Returns a reference to actual contained value.
+   *
+   * \warning Will abort if no value has been set
+   *
+   * \return
+   * A reference to the contained value
+   *
+   * \see OptionalValue::operator bool()
+   * \see OptionalValue::HasValue()
+   */
+  const T &GetValue (void) const;
+
+  /**
+   * Checks if the contained std::optional
+   * has a value. Should be checked before
+   * attempting to read the value.
+   *
+   * The method counterpart to the bool operator.
+   *
+   * \return
+   * True if the contained std::optional has a value,
+   * false otherwise.
+   *
+   * \see OptionalValue<T>::operator bool (void)
+   */
   bool HasValue (void) const;
+
+  /**
+   * Operator allowing the attribute to appear in conditionals.
+   *
+   * Checks if the contained std::optional holds a value.
+   *
+   * Allows for usage like the below:
+   * \code
+   * OptionalValue<int> hasValue{42};
+   * if (hasValue)
+   *   std::cout << "Will Print\n";
+   *
+   * OptionalValue<int> hasNoValue;
+   * if (hasNoValue)
+   *   std::cout << "Will Not Print\n";
+   * \endcode
+   *
+   * \return
+   * True if the contained `std::optional` has a value,
+   * false otherwise.
+   *
+   * \see OptionalValue::HasValue()
+   */
   explicit operator bool (void) const;
 
-  template <typename U>
-  T ValueOr (U &&defaultValue) const;
+  template <typename V>
+  bool GetAccessor (V &value) const;
 
-  void Reset ();
+  /**
+   * Copies the attribute with the enclosed value.
+   * Does not detect if the enclosed value is reference type.
+   *
+   * \return
+   * A copy of this attribute
+   */
+  Ptr<AttributeValue> Copy (void) const override;
+
+  /**
+   * Unimplemented conversion to string.
+   *
+   * Required by `AttributeValue`.
+   *
+   * \warning Will abort if called
+   *
+   * \param checker
+   * Unused
+   *
+   * \return
+   * Empty String
+   */
+  std::string SerializeToString (Ptr<const AttributeChecker> checker) const override;
+
+  /**
+   * Unimplemented conversion from string.
+   *
+   * Required by `AttributeValue`.
+   *
+   * \warning Will abort if called
+   *
+   * \param value
+   * Unused
+   *
+   * \param checker
+   * Unused
+   *
+   * \return
+   * False
+   */
+  bool DeserializeFromString (std::string value, Ptr<const AttributeChecker> checker) override;
 
 private:
-  using m_value_t = typename std::aligned_storage<sizeof (T), alignof (T)>::type;
-  m_value_t m_value;
-  bool m_initialized{false};
+  /**
+   * The backing store for the attribute
+   */
+  std::optional<T> m_value;
 };
 
 template <typename T>
-Optional<T> &
-Optional<T>::operator= (const Optional &other)
+OptionalValue<T>::OptionalValue (const std::optional<T> &value) : m_value{value}
 {
-  Reset ();
-
-  m_initialized = other.m_initialized;
-  if (other.m_initialized)
-    m_value = other.m_value;
-
-  return *this;
-}
-
-template <typename T>
-Optional<T> &
-Optional<T>::operator= (T &&other)
-{
-  Reset ();
-
-  new (&m_value) T (std::forward<T> (other));
-  m_initialized = true;
-
-  return *this;
-}
-
-template <typename T>
-template <class... Args>
-void
-Optional<T>::Emplace (Args &&...args)
-{
-  Reset ();
-
-  new (&m_value) T (std::forward<Args> (args)...);
-  m_initialized = true;
 }
 
 template <typename T>
 template <typename... Args>
-Optional<T>::Optional (Args &&...args) : m_initialized{true}
-{
-  new (&m_value) T (std::forward<Args> (args)...);
-}
-
-template <typename T>
-Optional<T>::~Optional ()
-{
-  Reset ();
-}
-
-template <typename T>
-Optional<T>::operator bool (void) const
-{
-  return HasValue ();
-}
-
-template <typename T>
-bool
-Optional<T>::HasValue (void) const
-{
-  return m_initialized;
-}
-
-template <typename T>
-const T &
-Optional<T>::GetValue (void) const
-{
-  NS_ABORT_MSG_UNLESS (m_initialized, "Attempted to access uninitialized Optional");
-  return *reinterpret_cast<const T *> (&m_value);
-}
-template <typename T>
-const T &
-Optional<T>::operator* (void) const
-{
-  return GetValue ();
-}
-
-template <typename T>
-template <typename U>
-T
-Optional<T>::ValueOr (U &&defaultValue) const
-{
-  return m_initialized ? GetValue () : static_cast<T> (std::forward<U> (defaultValue));
-}
-
-template <typename T>
-void
-Optional<T>::Reset ()
-{
-  if (!m_initialized)
-    return;
-
-  // Call T's destructor
-  reinterpret_cast<T *> (&m_value)->~T ();
-
-  // Just in case this gets called multiple times
-  m_initialized = false;
-}
-
-// ----- ATTRIBUTE -----
-
-template <typename T>
-class OptionalValue : public AttributeValue
-{
-  friend Optional<T>;
-
-public:
-  OptionalValue (void) = default;
-  OptionalValue (const OptionalValue<T> &other) = default;
-  OptionalValue (const Optional<T> &value);
-
-  template <typename... Args>
-  explicit OptionalValue (Args &&...args);
-
-  void Set (const Optional<T> &value);
-
-  template <typename... Args>
-  void Emplace (Args &&...args);
-
-  const Optional<T> &Get (void) const;
-  const T &GetValue (void) const;
-
-  explicit operator bool (void) const;
-
-  template <class V>
-  bool
-  GetAccessor (V &value) const
-  {
-    value = V (m_value);
-    return true;
-  }
-  Ptr<AttributeValue> Copy (void) const override;
-  std::string SerializeToString (Ptr<const AttributeChecker> checker) const override;
-  bool DeserializeFromString (std::string value, Ptr<const AttributeChecker> checker) override;
-
-private:
-  Optional<T> m_value;
-};
-
-template <typename T>
-OptionalValue<T>::OptionalValue (const Optional<T> &value) : m_value{value}
-{
-}
-
-template <typename T>
-template <class... Args>
-OptionalValue<T>::OptionalValue (Args &&...args) : m_value{std::forward<Args> (args)...}
+OptionalValue<T>::OptionalValue (Args &&...args)
+    : m_value{std::in_place, std::forward<Args> (args)...}
 {
 }
 
 template <typename T>
 void
-OptionalValue<T>::Set (const Optional<T> &value)
+OptionalValue<T>::Set (const std::optional<T> &value)
 {
   m_value = value;
 }
@@ -242,11 +263,11 @@ template <typename... Args>
 void
 OptionalValue<T>::Emplace (Args &&...args)
 {
-  m_value.template Emplace (args...);
+  m_value.template emplace (std::forward<Args> (args)...);
 }
 
 template <typename T>
-const Optional<T> &
+const std::optional<T> &
 OptionalValue<T>::Get (void) const
 {
   return m_value;
@@ -256,13 +277,29 @@ template <typename T>
 const T &
 OptionalValue<T>::GetValue (void) const
 {
-  return m_value.GetValue ();
+  return m_value.value ();
+}
+
+template <typename T>
+bool
+OptionalValue<T>::HasValue (void) const
+{
+  return m_value.has_value ();
 }
 
 template <typename T>
 OptionalValue<T>::operator bool (void) const
 {
-  return m_value.HasValue ();
+  return HasValue ();
+}
+
+template <typename T>
+template <typename V>
+bool
+OptionalValue<T>::GetAccessor (V &value) const
+{
+  value = V (m_value);
+  return true;
 }
 
 template <typename T>
@@ -276,16 +313,13 @@ template <typename T>
 std::string
 OptionalValue<T>::SerializeToString (Ptr<const AttributeChecker> checker) const
 {
-  // TODO: Not really sure I want to add this restriction for all types. How necessary is it?
   NS_ABORT_MSG ("Unsupported Operation 'SerializeToString()'");
   return {};
 }
-
 template <typename T>
 bool
 OptionalValue<T>::DeserializeFromString (std::string value, Ptr<const AttributeChecker> checker)
 {
-  // TODO: Same as `SerializeToString`, but more so
   NS_ABORT_MSG ("Unsupported Operation 'DeserializeFromString()'");
   return false;
 }
@@ -294,9 +328,9 @@ OptionalValue<T>::DeserializeFromString (std::string value, Ptr<const AttributeC
 
 template <typename T0, typename T1>
 Ptr<const AttributeAccessor>
-MakeOptionalAccessor (T1 a1)
+MakeOptionalAccessor (T1 getOrSet)
 {
-  return MakeAccessorHelper<OptionalValue<T0>> (a1);
+  return MakeAccessorHelper<OptionalValue<T0>> (getOrSet);
 }
 
 template <typename T0, typename T1, typename T2>
@@ -308,58 +342,118 @@ MakeOptionalAccessor (T1 setter, T2 getter)
 
 // ----- ATTRIBUTE CHECKER -----
 
+/**
+ * Rudimentary checker for optional attributes.
+ * Checks if an Attribute is both an OptionalAttribute
+ * and encloses the same value.
+ *
+ * \tparam T
+ * The enclosed type of the attribute to check for. It should
+ * match the template parameter of the OptionalAttribute
+ */
 template <typename T>
 class OptionalChecker : public AttributeChecker
 {
 public:
-  bool
-  Check (const AttributeValue &value) const override
-  {
-    return dynamic_cast<const OptionalValue<T> *> (&value) != nullptr;
-  }
+  /**
+   * Checks if `value` is an OptionalAttribute enclosing the
+   * correct type.
+   *
+   * \param value
+   * The attribute to check
+   *
+   * \return
+   * True if the attribute is an OptionalAttribute enclosing
+   * type `T` , false otherwise
+   */
+  bool Check (const AttributeValue &value) const override;
 
-  std::string
-  GetValueTypeName (void) const override
-  {
-    return "ns3::visualizer3d::OptionalValue";
-  }
+  /**
+   * Notifies ns-3 that we do not have human readable type info
+   *
+   * \return
+   * False
+   */
+  bool HasUnderlyingTypeInformation (void) const override;
 
-  bool
-  HasUnderlyingTypeInformation (void) const override
-  {
-    return false;
-  }
+  /**
+   * Pulls the name of `T` from `typeid`.
+   * Will produce a name, but will probably not be human readable
+   *
+   * \return
+   * The type name from the implementation
+   *
+   */
+  std::string GetUnderlyingTypeInformation (void) const override;
 
-  std::string
-  GetUnderlyingTypeInformation (void) const override
-  {
-    // Best possible answer I can give here
-    // Probably will not produce a readable name
-    // under GCC or Clang
-    return std::string{"ns3::visualizer3d::Optional<"} + typeid (T).name () + ">";
-  }
+  /**
+   * Create an uninitialized `OptionalChecker`
+   *
+   * \return
+   * A new `OptionalChecker`
+   */
+  Ptr<AttributeValue> Create (void) const override;
 
-  Ptr<AttributeValue>
-  Create (void) const override
-  {
-    // Qualify with the namespace since the
-    // function names are the same...
-    return ns3::Create<OptionalValue<T>> ();
-  }
-
-  bool
-  Copy (const AttributeValue &source, AttributeValue &destination) const override
-  {
-    const auto *src = dynamic_cast<const OptionalValue<T> *> (&source);
-    auto *dst = dynamic_cast<OptionalValue<T> *> (&destination);
-
-    if (src == nullptr || dst == nullptr)
-      return false;
-
-    *dst = *src;
-    return true;
-  }
+  bool Copy (const AttributeValue &source, AttributeValue &destination) const override;
 };
+
+template <typename T>
+bool
+OptionalChecker<T>::Check (const AttributeValue &value) const
+{
+  return dynamic_cast<const OptionalValue<T> *> (&value) != nullptr;
+}
+
+template <typename T>
+std::string
+OptionalChecker<T>::GetValueTypeName (void) const
+{
+  return "ns3::visualizer3d::OptionalValue";
+}
+
+template <typename T>
+bool
+OptionalChecker<T>::HasUnderlyingTypeInformation (void) const
+{
+  return false;
+}
+
+template <typename T>
+std::string
+OptionalChecker<T>::GetUnderlyingTypeInformation (void) const
+{
+  // Best possible answer I can give here
+  // Probably will not produce a readable name
+  // under GCC or Clang
+  // TODO: Maybe demangle this if people are really interested
+  // See: abi::__cxa_demangle
+  return std::string{"ns3::visualizer3d::Optional<"} + typeid (T).name () + ">";
+}
+
+template <typename T>
+Ptr<AttributeValue>
+OptionalChecker<T>::Create (void) const
+{
+  // Almost identical implementation to the helper
+
+  // Qualify with the namespace since the
+  // function names are the same...
+  return ns3::Create<OptionalValue<T>> ();
+}
+
+template <typename T>
+bool
+OptionalChecker<T>::Copy (const AttributeValue &source, AttributeValue &destination) const
+{
+  const auto *src = dynamic_cast<const OptionalValue<T> *> (&source);
+  auto *dst = dynamic_cast<OptionalValue<T> *> (&destination);
+
+  if (src == nullptr || dst == nullptr)
+    return false;
+
+  *dst = *src;
+  return true;
+}
 
 template <typename T>
 Ptr<const AttributeChecker>
@@ -368,9 +462,8 @@ MakeOptionalChecker (void)
   return Create<OptionalChecker<T>> ();
 }
 
-// TODO: Bounded Checker for Number types
+// TODO: Maybe Bounded Checker for Number types
 
-} // namespace visualizer3d
-} // namespace ns3
+} // namespace ns3::visualizer3d
 
 #endif // OPTIONAL_H
