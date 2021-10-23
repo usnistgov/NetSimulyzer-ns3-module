baaed00fcdc68c912adf854417ab83651a4c0773
Evan Black <evan.black@nist.gov>
2021-10-22 21:50:29 -0400

    Version 1.0.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'netsimulyzer'
+project = 'NetSimulyzer'
 # copyright = '2020, NIST'
 author = 'Evan Black'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3-pre'
+release = '1.0.3-release'
 
 
 # -- General configuration ---------------------------------------------------
