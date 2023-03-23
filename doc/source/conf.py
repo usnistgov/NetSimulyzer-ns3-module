c4f9eab6daf816935b9fdba8c0154244d1f23c93
Evan Black <evan.black@nist.gov>
2023-03-23 12:23:14 -0400

    Start version 1.0.7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ project = 'NetSimulyzer'
 author = 'Evan Black'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.6-release'
+release = '1.0.7-pre'
 
 
 # -- General configuration ---------------------------------------------------
