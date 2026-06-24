47812e3ab16322b296aaef742a6f073c4fc197a5
Evan Black <evan.black@nist.gov>
2026-06-24 17:06:15 -0400

    Start version 1.0.18
    # Conflicts:
    #	doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ project = 'NetSimulyzer'
 author = 'Evan Black'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.16-pre'
+release = '1.0.18-pre'
 
 
 # -- General configuration ---------------------------------------------------
